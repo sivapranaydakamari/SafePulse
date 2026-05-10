cc6cfa94f9f3736d7809193715a04d2b6a37b99f
naveenkumar6788 <naveenkumardasari6788@gmail.com>
2026-05-10 11:25:34 +0530

    Fix base url formatting

--- frontend/lib/core/config/app_config.dart
+++ frontend/lib/core/config/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   static const String baseUrl = String.fromEnvironment(
     'BASE_URL',
-    defaultValue: 'http://10.212.213.239:5000/api', 
+    defaultValue: 'http://10.212.213.239:5000', 
   );
 }
